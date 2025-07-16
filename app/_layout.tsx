f286155b3a10f0a93dd40d6cc932070eed2c4288
boterop <boterop22@gmail.com>
2025-07-15 23:25:03 -0500

    Dark theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,12 +10,15 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
 
+import { useColorScheme } from "@/hooks/useColorScheme";
+
 import "./globals";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -31,7 +34,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <StatusBar style="auto" />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
